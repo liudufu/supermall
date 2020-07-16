7bb1ee2005018552657863dd92c59fc09a1e5196
语心 <2649176532@qq.com>
2020-07-16 14:14:35 +0800

    'add_profile_page'

--- src/views/profile/Profile.vue
+++ src/views/profile/Profile.vue
@@ -1,70 +1,53 @@
 <template>
-  <div>
-    <ul>
-      <li>列表1</li>
-      <li>列表2</li>
-      <li>列表3</li>
-      <li>列表4</li>
-      <li>列表5</li>
-      <li>列表6</li>
-      <li>列表7</li>
-      <li>列表8</li>
-      <li>列表9</li>
-      <li>列表10</li>
-      <li>列表11</li>
-      <li>列表12</li>
-      <li>列表13</li>
-      <li>列表14</li>
-      <li>列表15</li>
-      <li>列表16</li>
-      <li>列表17</li>
-      <li>列表18</li>
-      <li>列表19</li>
-      <li>列表20</li>
-      <li>列表21</li>
-      <li>列表22</li>
-      <li>列表23</li>
-      <li>列表24</li>
-      <li>列表25</li>
-      <li>列表26</li>
-      <li>列表27</li>
-      <li>列表28</li>
-      <li>列表29</li>
-      <li>列表30</li>
-      <li>列表31</li>
-      <li>列表32</li>
-      <li>列表33</li>
-      <li>列表34</li>
-      <li>列表35</li>
-      <li>列表36</li>
-      <li>列表37</li>
-      <li>列表38</li>
-      <li>列表39</li>
-      <li>列表40</li>
-      <li>列表41</li>
-      <li>列表42</li>
-      <li>列表43</li>
-      <li>列表44</li>
-      <li>列表45</li>
-      <li>列表46</li>
-      <li>列表47</li>
-      <li>列表48</li>
-      <li>列表49</li>
-      <li>列表50</li>
-    </ul>
+  <div id="profile">
+    <nav-bar class="nav-bar">
+      <div slot="center">我的档案</div>
+    </nav-bar>
+    <user-info/>
+    <account-info/>
+    <normal-list-view :list-data="orderList"></normal-list-view>
+    <normal-list-view :list-data="serviceList"></normal-list-view>
   </div>
 </template>
 
 <script>
-  import Scroll from "components/common/scroll/Scroll";
+  import NavBar from "components/common/navbar/NavBar";
+
+  import UserInfo from "./childComponents/UserInfo";
+  import AccountInfo from "./childComponents/AccountInfo";
+  import NormalListView from "./childComponents/NormalListView";
+
   export default {
     name: "Profile",
     components: {
-      Scroll
+      NavBar,
+      UserInfo,
+      AccountInfo,
+      NormalListView
+    },
+    data() {
+      return {
+        orderList: [
+          {icon: 'message.svg', info: '我的消息'},
+          {icon: 'pointer.svg', info: '积分商城'},
+          {icon: 'vip.svg', info: '会员卡'},
+        ],
+        serviceList: [
+          {icon: 'cart.svg', info: '我的购物车'},
+          {icon: 'shopping.svg', info: '下载购物APP'},
+        ]
+      }
     }
   }
 </script>
 
 <style scoped>
+  #profile {
+    background-color: #f2f2f2;
+  }
 
+  .nav-bar {
+    background-color: var(--color-tint);
+    color: #fff;
+  }
 </style>
